ccd3217dc027869c1831dea695e6a39363be7d90
thebin <1974911747@qq.com>
2022-02-28 16:20:23 +0800

    AC v2: STL map

--- Basic_level/1100校庆.cpp
+++ Basic_level/1100校庆.cpp
@@ -4,60 +4,30 @@
 #include<map>
 using namespace std;
 
-
 int main() {
-	int n;
-	cin >> n;
 
-	string s;
-	string s1 = "99999999";
-	string s2 = "99999999";
-	string s3 = "99999999";
-	string val1 = ""; //记录来宾中最年长校友
-	string val2 = ""; //记录最年长来宾
-	string val3 = ""; //记录最年长校友
-
-	//存储校友身份info
-	map<string, bool> mp;
+	int n; cin >> n;
+	string info;
+	map<string, string> mymap, hpy, host; //存储校友的<身份信息，出生日期>
 	for (int i = 0; i < n; i++) {
-		string tmp;
-		cin >> tmp;
-		mp[tmp] = true;
-		string tmp_date = tmp.substr(6, 8);
-		if (tmp_date < s3) {
-			s3 = tmp_date;
-			val3 = tmp;
-		}
+		cin >> info;
+		mymap[info] = info.substr(6, 8);
 	}
-
-	int c;
-	cin >> c;
-	bool flag = false;
-	int ans = 0;
+	int c; cin >> c;
 	for (int i = 0; i < c; i++) {
-		cin >> s; //获得来宾信息
-		if (mp[s]) {//如果来宾是校友，ans++，并输出最年长的；
-			ans++;
-			flag = true;
-			string birth_data1 = s.substr(6, 8);
-			if (birth_data1 < s1) {
-				s1 = birth_data1;
-				val1 = s;
-			}
-		}
-		else {//来宾中没有校友，输出来宾中最年长的；
-			string birth_data2 = s.substr(6, 8);
-			if (birth_data2 < s2) {
-				s2 = birth_data2;
-				val2 = s;
-			}
-		}
+		cin >> info;
+		//如果来宾中有校友的话，将信息存放在hpy中
+		//与此同时，将所有来宾信息都存放在host中
+		//Attention:map会根据key排序，而非value
+		if (mymap[info] != "")hpy[info.substr(6, 8)] = info;
+		host[info.substr(6, 8)] = info;
 	}
-	cout << ans << endl;
-	if (flag) {
-		if (val1 > val3) cout << val1 << endl;
-		else cout << val3 << endl;
+	//如果hpy为空，也就是来宾中没有一个校友，那么直接输出host中最年长的；
+	//反之，输出hpy中最年长的
+	if (hpy.size()) {
+		cout << hpy.size() << endl;
+		cout << hpy.begin()->second << endl;
 	}
-	else cout << val2 << endl;
+	else cout << host.begin()->second << endl;
 	return 0;
 }
